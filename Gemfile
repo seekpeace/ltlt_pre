a4a92cd28db445602bd3bfbf94a87b2558b1087b
Enoch Shin <seekpeace@gmail.com>
2015-05-13 11:01:20 -0400

    worked on css polish

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-source 'https://rubygems.org'
 
+gem 'bootstrap-sass'
 gem 'devise'
 gem 'pry'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,26 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'static_pages/home'
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/help'
 
-#set default homepage
-
-  # devise_for :installs
-  # devise_for :user
   resources :users
-
+  get 'signup' => 'users#new'
 
 end
+
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get  'static_pages/contact' <=this works with link_to static_pages_contact_path
+
+  # get 'static_pages/home'
+  #
+  # get 'static_pages/contact'
+  #
+  # get 'static_pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
